d31162acd67efd95f69abc2f77a5f9afd0504f2c
houssein hershy <hhershy@hotmail.com>
2022-05-28 00:37:58 +0300

    Enable Seed

--- Services/EShop.API/Program.cs
+++ Services/EShop.API/Program.cs
@@ -1,3 +1,5 @@
+using EShop.API.Extensions;
+using EShop.Infrastracture.Persistance;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,7 +15,13 @@ namespace EShop.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build()
+                .MigrateDatabase<ProductContext>((context, services) =>
+                {
+                    ProductContextSeed.InitializeAsync(context)
+                      .Wait();
+                })
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ Services/EShop.Infrastracture/Persistance/ProductContextSeed.cs
@@ -0,0 +1,39 @@
+﻿using EShop.Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Infrastracture.Persistance
+{
+    public static class ProductContextSeed
+    {
+        public static async Task InitializeAsync(ProductContext productContext)
+        {
+            if (!productContext.Products.Any())
+            {
+                productContext.Products.AddRange(GetPreconfiguredProduct());
+                await productContext.SaveChangesAsync();
+            }
+
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProduct()
+        {
+            return new List<Product>
+            {
+                new Product() { Name = "PC", CategoryName = "Elec", Color ="Green", Description = "test",  CreatedBy = "Herchi", CreatedDate = DateTime.Now ,
+                    ProdutPriceDetails = new ProductDetail(){
+                    Price = 100,
+                     ActualCost = 90
+                }}, 
+                new Product() {Name = "Laptop", CategoryName = "Elecc",Color ="Yellow",Description = "Test01",CreatedBy = "HHerchi", CreatedDate = DateTime.Now ,
+                ProdutPriceDetails = new ProductDetail(){
+                    Price = 100,
+                     ActualCost = 90
+                }}
+            };
+        }
+    }
+}
