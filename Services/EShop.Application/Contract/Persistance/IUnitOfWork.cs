a8f0f7a1cff0888fe4e17c9fcd1dc45242b7b2cc
houssein hershy <hhershy@hotmail.com>
2022-05-27 22:39:20 +0300

    Infrastracture Contracts

---
+++ Services/EShop.Application/Contract/Persistance/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EShop.Application.Contract.Persistance
+{
+    public interface IUnitOfWork : IDisposable
+    {
+       
+      
+        int Complete();
+        Task<int> CompleteAsync();
+
+
+    }
+}
